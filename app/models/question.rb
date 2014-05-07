b1a5c399cb2ae6d83d0da0e9d58e63ff7a7af342
Vitaly Kurennov <vkurennov@gmail.com>
2014-05-07 22:45:11 +0600

    Add association between question and answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
+  has_many :answers
+
   validates :title, :body, presence: true
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Question do
+  it { should have_many :answers }
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
