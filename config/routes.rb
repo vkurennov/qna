645d41cda9b4e9712b5f5db07ecbef767d99861b
Vitaly Kurennov <vkurennov@gmail.com>
2014-10-05 12:26:16 +0600

    New routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,11 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  resources :questions, concerns: :commentable do
-    resources :answers
+  resources :questions, shallow: true, concerns: :commentable do
+    resources :answers, concerns: :commentable
   end
 
-  resources :answers, only: [], concerns: :commentable
+  #resources :answers, only: [], concerns: :commentable
 
   namespace :api do
     namespace :v1 do
